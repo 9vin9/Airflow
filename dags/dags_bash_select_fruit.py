adf93789174412b3b3eb8c061a5fa093398053e9
9vin9 <luvin3994@gmail.com>
2024-03-21 23:35:16 +0900

    select_fruit.sh 수행하는 dag 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -1,9 +1,9 @@
-# DAG 정의
 from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.bash import BashOperator
 
+# DAG 정의
 with DAG(
     dag_id = 'dags_bash_select_fruit',
     schedule = '10 0 * * 6#1', # 매월 첫째주 토요일 0시 10분
@@ -13,12 +13,12 @@ with DAG(
     
     t1_orange = BashOperator(
         task_id = 't1_orange',
-        bash_command="/opt/airflow/airflow/plugins/shell/select_fruit.sh ORANGE",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
 
     t2_avocado = BashOperator(
         task_id = 't2_avocado',
-        bash_command="/opt/airflow/airflow/plugins/shell/select_fruit.sh AVOCADO",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
     t1_orange >> t2_avocado
